3079d2f56b48729fa28030519ed3755bf2863aec
Pedro Henrique <preisteixeira@gmail.com>
2024-09-12 11:41:53 -0400

    banco de dados configurado

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title: models.CharField(max_length=100,null=False,blank=False)
+    description: models.TextField(null=False,blank=False)
+    autor: models.CharField(max_length=100,null=False,blank=False)
+    date: models.DateField()
+    tags = models.ManyToManyField('Tag')
+    coments = models.ManyToManyField('Coments')
+
+    def __str__(self):
+        return self.title
+    
+class Tag(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class Coments(models.Model):
+    autor = models.CharField(max_length=100,null=False,blank=False)
+    coment = models.TextField(null=False,blank=False)
+
+    def __str__(self):
+        return self.autor
